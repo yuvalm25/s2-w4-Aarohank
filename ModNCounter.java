8fab7f2616984d4d2f685b26f710f902b274f443
Aarohank <aarohank21@gmail.com>
2024-01-22 12:15:22 -0800

    S2W4W1

--- ModNCounter.java
+++ ModNCounter.java
@@ -1,7 +1,17 @@
 
- public class ModNCounter{
-    //Put instance variables here
+ public class ModNCounter extends Counter {
+   //Put instance variables here
    
+   public ModNCounter(){
+      super();
+   }
+
+   public void increment(int lim){
+      super.increment();
+      if(super.value() >= lim){
+         super.reset();
+      }
+   }
    
    
    //Add additional methods to provide functionality to make the counter count in terms of "mod N"
